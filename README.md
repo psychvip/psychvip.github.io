fd57b0d857b0e7776234303b5f01a916db278389
2lag <aidenhelfman@gmail.com>
2023-02-05 01:43:40 -0600

    more functionality

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # psychvip.github.io
-1. register and 4got pwd? <A> tags into buttons
-* 1. call js function to add email input & set to only email input respectively
--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 function showLogin() {
     let logincontainer = document.getElementById("login-container");
-    if(logincontainer.style.display === "none") {
+    if (logincontainer.style.display === "none") {
         logincontainer.style.display = "block";
     } else {
         logincontainer.style.display = "none";
     }
 }
+
+function forgotPassword() {
+    if (document.getElementById("pass-input").style.display === "none") {
+        document.getElementById("user-input").placeholder = "mail/user";
+        document.getElementById("pass-input").style.display = "inline-block";
+        document.getElementById("email-input").style.display = "none";
+        document.getElementById("invite-input").style.display = "none";
+        document.getElementById("submit-btn").innerText = "login";
+    } else {
+        document.getElementById("pass-input").style.display = "none";
+        document.getElementById("email-input").style.display = "none";
+        document.getElementById("invite-input").style.display = "none";
+        document.getElementById("user-input").placeholder = "mail/user";
+        document.getElementById("submit-btn").innerText = "reset";
+    }
+}
+
+function showRegister() {
+    if (document.getElementById("email-input").style.display === "none" || document.getElementById("invite-input").style.display === "none") {
+        document.getElementById("email-input").style.display = "inline-block";
+        document.getElementById("invite-input").style.display = "inline-block";
+        document.getElementById("pass-input").style.display = "inline-block";
+        document.getElementById("user-input").placeholder = "user";
+        document.getElementById("submit-btn").innerText = "make acc";
+    } else {
+        document.getElementById("email-input").style.display = "none";
+        document.getElementById("invite-input").style.display = "none";
+        document.getElementById("pass-input").style.display = "inline-block";
+        document.getElementById("user-input").placeholder = "mail/user";
+        document.getElementById("submit-btn").innerText = "login";
+    }
+}
+document.getElementById("forgot-btn").addEventListener("click", forgotPassword);
+document.getElementById("register-btn").addEventListener("click", showRegister);
