298e46ba3082bb32ce22db8dbacb6f9ab7646089
2lag <aidenhelfman@gmail.com>
2023-02-05 21:19:37 -0600

    update login

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # psychvip.github.io
-* switch login-container to a class in css and html files
 * make each different login form have its own id
 * * register, login, forgot
 * make each new one appear on button press for better individual styling
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+let featsOpen = false, videoOpen = false;
+let loginOpen = false, registerOpen = false, forgotOpen = false;
+
 function openFeats() {
     let feats = document.getElementsByClassName("features")[0];
     let hidebtn = document.getElementsByClassName("hide-btn")[0];
     if (feats.style.left === "-160px") {
+        featsOpen = true;
         feats.style.left = "0px";
         hidebtn.style.left = "155px";
         hidebtn.innerHTML = "&lt;-";
     } else {
+        featsOpen = false;
         feats.style.left = "-160px";
         hidebtn.style.left = "-5px";
         hidebtn.innerHTML = "-&gt;";
     }
+    console.log("featsOpen: " + featsOpen);
 }
 
 function showLogin() {
@@ -19,10 +25,13 @@ function showLogin() {
         if(videocontainer.style.display === "block") {
             videocontainer.style.display = "none";
         }
+        loginOpen = true;
         logincontainer.style.display = "block";
     } else {
+        loginOpen = false;
         logincontainer.style.display = "none";
     }
+    console.log("loginOpen: " + loginOpen);
 }
 
 function showVideo() {
@@ -32,43 +41,60 @@ function showVideo() {
         if(logincontainer.style.display === "block") {
             logincontainer.style.display = "none";
         }
+        videoOpen = true;
         videocontainer.style.display = "block";
     } else {
+        videoOpen = false;
         videocontainer.style.display = "none";
     }
+    console.log("videoOpen: " + videoOpen);
 }
 
 function forgotPassword() {
     if (document.getElementById("pass-input").style.display === "none") {
-        document.getElementById("user-input").placeholder = "mail/user";
+        forgotOpen = false;
+        loginOpen = true;
+        document.getElementById("user-input").placeholder = "email/user";
         document.getElementById("pass-input").style.display = "inline-block";
         document.getElementById("email-input").style.display = "none";
         document.getElementById("invite-input").style.display = "none";
+        document.getElementById("login-container").style.height = "285px";
         document.getElementById("submit-btn").innerText = "login";
     } else {
+        forgotOpen = true;
+        loginOpen = false;
         document.getElementById("pass-input").style.display = "none";
         document.getElementById("email-input").style.display = "none";
         document.getElementById("invite-input").style.display = "none";
-        document.getElementById("user-input").placeholder = "mail/user";
+        document.getElementById("user-input").placeholder = "email/user";
+        document.getElementById("login-container").style.height = "240px";
         document.getElementById("submit-btn").innerText = "reset";
     }
+    console.log("forgotOpen: " + forgotOpen);
 }
 
 function showRegister() {
     if (document.getElementById("email-input").style.display === "none" || document.getElementById("invite-input").style.display === "none") {
+        registerOpen = true;
+        loginOpen = false;
         document.getElementById("email-input").style.display = "inline-block";
         document.getElementById("invite-input").style.display = "inline-block";
         document.getElementById("pass-input").style.display = "inline-block";
         document.getElementById("user-input").placeholder = "user";
+        document.getElementById("login-container").style.height = "400px";
         document.getElementById("submit-btn").innerText = "make acc";
     } else {
+        registerOpen = false;
+        loginOpen = true;
         document.getElementById("email-input").style.display = "none";
         document.getElementById("invite-input").style.display = "none";
         document.getElementById("pass-input").style.display = "inline-block";
-        document.getElementById("user-input").placeholder = "mail/user";
+        document.getElementById("user-input").placeholder = "email/user";
+        document.getElementById("login-container").style.height = "285px";
         document.getElementById("submit-btn").innerText = "login";
     }
+    console.log("registerOpen: " + registerOpen);
 }
 
-document.getElementById("forgot-btn").addEventListener("click", forgotPassword);
 document.getElementById("register-btn").addEventListener("click", showRegister);
+document.getElementById("forgot-btn").addEventListener("click", forgotPassword);
