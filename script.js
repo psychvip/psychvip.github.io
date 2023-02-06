dd3ec76c21be6ae0801e7e217754a632779bb01c
2lag <aidenhelfman@gmail.com>
2023-02-06 00:07:49 -0600

    final js changes b4 login finish

--- script.js
+++ script.js
@@ -20,9 +20,10 @@ function openFeats() {
 
 //if window is resized and feats are open and window is less than 671px, close feats
 window.addEventListener("resize", function() {
+    let windowOpen = videoOpen || loginOpen || registerOpen || forgotOpen;
     let feats = document.getElementsByClassName("features")[0];
     let hidebtn = document.getElementsByClassName("hide-btn")[0];
-    if (featsOpen && window.innerWidth <= 671) {
+    if (featsOpen && window.innerWidth <= 671 && windowOpen) {
         featsOpen = false;
         feats.style.left = "-160px";
         hidebtn.style.left = "-5px";
@@ -33,15 +34,15 @@ window.addEventListener("resize", function() {
 function showLogin() {
     let logincontainer = document.getElementById("login-container");
     let videocontainer = document.getElementById("video-container");
-    if (logincontainer.style.display === "none") {
+    if (logincontainer.style.display === "block") {
+        loginOpen = false;
+        logincontainer.style.display = "none";
+    } else {
         if(videocontainer.style.display === "block") {
             videocontainer.style.display = "none";
         }
         loginOpen = true;
         logincontainer.style.display = "block";
-    } else {
-        loginOpen = false;
-        logincontainer.style.display = "none";
     }
     console.log("loginOpen: " + loginOpen);
 }
@@ -49,15 +50,15 @@ function showLogin() {
 function showVideo() {
     let logincontainer = document.getElementById("login-container");
     let videocontainer = document.getElementById("video-container");
-    if (videocontainer.style.display === "none") {
+    if (videocontainer.style.display === "block") {
+        videoOpen = false;
+        videocontainer.style.display = "none";
+    } else {
         if(logincontainer.style.display === "block") {
             logincontainer.style.display = "none";
         }
         videoOpen = true;
         videocontainer.style.display = "block";
-    } else {
-        videoOpen = false;
-        videocontainer.style.display = "none";
     }
     console.log("videoOpen: " + videoOpen);
 }
@@ -72,6 +73,8 @@ function forgotPassword() {
         document.getElementById("invite-input").style.display = "none";
         document.getElementById("login-container").style.height = "285px";
         document.getElementById("submit-btn").innerText = "login";
+        document.getElementById("forgot-btn").innerText = "4got pwd?";
+        document.getElementById("register-btn").innerText = "register";
     } else {
         forgotOpen = true;
         loginOpen = false;
@@ -81,6 +84,8 @@ function forgotPassword() {
         document.getElementById("user-input").placeholder = "email/user";
         document.getElementById("login-container").style.height = "240px";
         document.getElementById("submit-btn").innerText = "reset";
+        document.getElementById("forgot-btn").innerText = "back";
+        document.getElementById("register-btn").innerText = "register";
     }
     console.log("forgotOpen: " + forgotOpen);
 }
@@ -95,6 +100,8 @@ function showRegister() {
         document.getElementById("user-input").placeholder = "user";
         document.getElementById("login-container").style.height = "400px";
         document.getElementById("submit-btn").innerText = "make acc";
+        document.getElementById("register-btn").innerText = "back";
+        document.getElementById("forgot-btn").innerText = "4got pwd?";
     } else {
         registerOpen = false;
         loginOpen = true;
@@ -104,9 +111,8 @@ function showRegister() {
         document.getElementById("user-input").placeholder = "email/user";
         document.getElementById("login-container").style.height = "285px";
         document.getElementById("submit-btn").innerText = "login";
+        document.getElementById("register-btn").innerText = "register";
+        document.getElementById("forgot-btn").innerText = "4got pwd?";
     }
     console.log("registerOpen: " + registerOpen);
 }
-
-document.getElementById("register-btn").addEventListener("click", showRegister);
-document.getElementById("forgot-btn").addEventListener("click", forgotPassword);
